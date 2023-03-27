32f521ba5054190a90b62f2b45c27e4afb98f899
Müller Csaba <csaba.muller@hotmail.com>
2023-03-27 22:58:37 +0200

    mobile version almost completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        /* Primary */
+        "strong-Cyan": "hsl(171, 66%, 44%)",
+        "light-Blue": "hsl(233, 100%, 69%)",  
+        /* Neutral */
+        "dark-Grayish-Blue": "hsl(210,10 %,33 %)",
+        "grayish-Blue": "hsl(201,11% ,66%)"
+      },
+    },
+    fontFamily: {
+      'sans': ["Bai Jamjuree", "sans-serif"],
+    },
   },
   plugins: [],
 }
